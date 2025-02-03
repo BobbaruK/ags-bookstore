8a09259e069e640072c83384cefea1d1e61433d2
Bobbaru <bobbaru_krueger@yahoo.com>
2025-02-03 22:41:49 +0200

    globals: stock alert update

--- webapp/src/components/stock-alert.tsx
+++ webapp/src/components/stock-alert.tsx
@@ -4,10 +4,22 @@ interface Props {
 
 export const StockAlert = ({ stock }: Props) => {
   if (stock > 10)
-    return <span className="block size-6 rounded-full bg-success"></span>;
+    return (
+      <span className="grid size-6 place-items-center rounded-full bg-success text-success-foreground">
+        {stock}
+      </span>
+    );
 
   if (stock > 5)
-    return <span className="block size-6 rounded-full bg-warning"></span>;
+    return (
+      <span className="grid size-6 place-items-center rounded-full bg-warning text-warning-foreground">
+        {stock}
+      </span>
+    );
 
-  return <span className="block size-6 rounded-full bg-danger"></span>;
+  return (
+    <span className="grid size-6 place-items-center rounded-full bg-danger text-danger-foreground">
+      {stock}
+    </span>
+  );
 };
